58b5f1d839573d3fc2765451f6a794d697bda0fb
Ravi Patel <ravihpatel181229@gmail.com>
2024-02-03 18:51:24 +0530

    validation improved

--- src/components/AddExpenseDialog.jsx
+++ src/components/AddExpenseDialog.jsx
@@ -8,8 +8,18 @@ function AddExpenseDialog({ updateBalance, item, deleteHistory, action }) {
         const desc = e.target[1].value
         const id = item ? item.id : Math.floor(Math.random() * 100000000)
 
-        if (!amount || !desc)
+        if (!amount || !desc) {
+            if (isNaN(amount)) {
+                alert("Please enter amount in numbers.")
+                return
+            }
+            if (e.target[0].value == "0") {
+                alert("Please enter Positive number as amount.")
+                return
+            }
             alert("Please enter required fields!!");
+            return
+        }
 
         updateBalance({ id, amount, desc })
     }
@@ -22,10 +32,10 @@ function AddExpenseDialog({ updateBalance, item, deleteHistory, action }) {
                 {action == "EXPENSE" ? "Expense" : "Amount"}
             </h1>
             <form action="" onSubmit={submitForm} className='flex flex-col gap-2 p-5'>
-                <input type="number" name='amount' min={0} placeholder='Amount' defaultValue={item ? Math.abs(item.amount) : 0}
-                    className='p-2 border border-slate-500' required />
+                <input type="sdg" name='amount' min={1} placeholder='Amount' defaultValue={item ? Math.abs(item.amount) : 0}
+                    className='p-2 border border-slate-500' />
                 <input type="text" name='desc' placeholder='Desciption' defaultValue={item ? item.desc : ""}
-                    className='p-2 border border-slate-500' required />
+                    className='p-2 border border-slate-500' />
                 {
                     item ?
                         <div className='md:grid grid-cols-2 gap-2 space-y-2 md:space-y-0'>
